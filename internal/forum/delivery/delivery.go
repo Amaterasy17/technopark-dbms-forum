14b6564dac7e7d329632c0bdbcbec6e9bd17ca09
Amaterasy17 <sheb70017@gmail.com>
2020-12-09 02:57:54 +0300

    Some fixing in delivery

--- internal/forum/delivery/delivery.go
+++ internal/forum/delivery/delivery.go
@@ -3,14 +3,14 @@ package delivery
 import (
 	"github.com/gorilla/mux"
 	"net/http"
-	domain "technopark-dbms-forum/internal/forum/usecase"
+	domain "technopark-dbms-forum/internal/forum"
 )
 
 type ForumHandler struct {
-	ForumUseCase domain.ForumUsecase
+	ForumUseCase domain.ForumUseCase
 }
 
-func NewForumHandler(r *mux.Router, forumUseCase domain.ForumUsecase) {
+func NewForumHandler(r *mux.Router, forumUseCase domain.ForumUseCase) {
 	handler := &ForumHandler{ForumUseCase: forumUseCase}
 
 	r.HandleFunc("/forum", handler.Forum).Methods(http.MethodPost)
